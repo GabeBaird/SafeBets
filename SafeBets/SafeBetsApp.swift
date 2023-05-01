f20ada5110ce8a8374db00eba1af780a159aeab1
Gabe Baird <gabebaird123@gmail.com>
2023-05-01 09:04:24 -0500

    Initial Commit

---
+++ SafeBets/SafeBetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeBetsApp.swift
+//  SafeBets
+//
+//  Created by Gabe Baird on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SafeBetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
